ebb07ef3c755c6c42da73ed27780b13602408746
John Nguyen <johnhvn94@gmail.com>
2019-03-18 15:20:04 -0700

    Add files via upload

--- finalproject/finalapp/views.py
+++ finalproject/finalapp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import EventType
 
 # Create your views here.
 def index(request):
     return render(request, 'finalapp/index.html')
+
+def eventtypes(request):
+    type_list=EventType.objects.all()
+    return render(request, 'finalapp/types.html', {'type_list': type_list})
