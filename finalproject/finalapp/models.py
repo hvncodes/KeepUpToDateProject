a8ccdd108a846c7c255f9bd03c78f93b1de4f2c3
John Nguyen <johnhvn94@gmail.com>
2019-03-22 03:42:05 -0700

    Update models.py

--- finalproject/finalapp/models.py
+++ finalproject/finalapp/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 # one off?, daily, weekly, monthly
-class EventType(models.Model):
+class TaskType(models.Model):
     typename=models.CharField(max_length=255)
     typedescription=models.CharField(max_length=255, null=True, blank=True)
 
@@ -11,26 +11,26 @@ class EventType(models.Model):
         return self.typename
     
     class Meta:
-        db_table='eventtype'
+        db_table='tasktype'
 
-class Event(models.Model):
-    eventname=models.CharField(max_length=255)
-    eventtype=models.ForeignKey(EventType, on_delete=models.DO_NOTHING)
+class Task(models.Model):
+    taskname=models.CharField(max_length=255)
+    tasktype=models.ForeignKey(TaskType, on_delete=models.DO_NOTHING)
     user=models.ForeignKey(User, on_delete=models.DO_NOTHING)
-    evententrydate=models.DateField()
-    eventurl=models.URLField(null=True, blank=True)
-    eventdescription=models.TextField()
+    taskentrydate=models.DateField()
+    taskurl=models.URLField(null=True, blank=True)
+    taskdescription=models.TextField()
 
     def __str__(self):
-        return self.eventname
+        return self.taskname
 
     class Meta:
-        db_table='event'
+        db_table='task'
 
 class Comment(models.Model):
     commenttitle=models.CharField(max_length=255)
     commentdate=models.DateField()
-    event=models.ForeignKey(Event, on_delete=models.DO_NOTHING)
+    task=models.ForeignKey(Task, on_delete=models.DO_NOTHING)
     user=models.ManyToManyField(User)
     commenttext=models.TextField()
 
