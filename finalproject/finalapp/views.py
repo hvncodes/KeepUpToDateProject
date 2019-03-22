1a7b886172535795927f7baf20c56b632802271c
John Nguyen <johnhvn94@gmail.com>
2019-03-22 03:43:20 -0700

    Update views.py

--- finalproject/finalapp/views.py
+++ finalproject/finalapp/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from .models import EventType
+from .models import TaskType
 
 # Create your views here.
 def index(request):
     return render(request, 'finalapp/index.html')
 
-def eventtypes(request):
-    type_list=EventType.objects.all()
+def tasktypes(request):
+    type_list=TaskType.objects.all()
     return render(request, 'finalapp/types.html', {'type_list': type_list})
