0c7031a7eab55c4f1fd15552cbc4368bd1467059
John Nguyen <johnhvn94@gmail.com>
2019-03-22 04:53:15 -0700

    Update tests.py

--- finalproject/finalapp/tests.py
+++ finalproject/finalapp/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import TaskType, Task, Comment
+from django.urls import reverse
 
+# python manage.py test
 # Create your tests here.
+class TaskTest(TestCase):
+  def test_stringOutput(self):
+    task=Task(typename='Daily')
+    self.assertEqual(str(Task), task.taskname)
+  
+  def test_tablename(self):
+    self.assertEqual(str(Task._meta.db_table), 'task')
+    
+Class TestIndex(TestCase):
+  def test_view_url_accessible_by_name(self):
+    response=self.client(reverse('index'))
+    self.assertEqual(response.status_code, 200)
