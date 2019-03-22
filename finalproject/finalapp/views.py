22b08616b78516f39aad974c4c01babb9a37c31b
John Nguyen <johnhvn94@gmail.com>
2019-03-22 06:24:49 -0700

    Update views.py

--- finalproject/finalapp/views.py
+++ finalproject/finalapp/views.py
@@ -8,3 +8,25 @@ def index(request):
 def tasktypes(request):
     type_list=TaskType.objects.all()
     return render(request, 'finalapp/types.html', {'type_list': type_list})
+
+
+
+#form view
+@login_required
+def newevent(request):
+    form=TypeForm
+    if request.method=='POST':
+        form=TypeForm(request.POST)
+        if form.is_valid():
+            post=form.save(commit=True)
+            post.save()
+            form=TypeForm()
+    else:
+        form=TypeForm()
+    return render(request, 'finalapp/newtask.html', {'form': form})
+
+def loginmessage(request):
+    return render(request, 'finalapp/loginmessage.html')
+
+def logoutmessage(request):
+    return render(request, 'finalapp/logoutmessage.html')
