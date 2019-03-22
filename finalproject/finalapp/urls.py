17d3c78c11c43b2898a0eacedb976f9c66f2b006
John Nguyen <johnhvn94@gmail.com>
2019-03-22 15:55:11 -0700

    Update urls.py

--- finalproject/finalapp/urls.py
+++ finalproject/finalapp/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns=[
     path('', views.index, name='index'),
     path('tasktypes/', views.tasktypes, name="tasktypes"),
-    #
+    path(‘gettasks/’, views.gettasks, name=’gettasks’),
+    path(‘taskdetail/<int:id>’, views.taskdetail, name=’taskdetail’),
     path('newTask/', views.newTask, name='newtask'),
     path('loginmessage/', views.loginmessage, name='loginmessage'),
     path('logoutmessage/', views.logoutmessage, name='logoutmessage'),
