6ad293395453bc9c0637e3e6f2d177935a00daac
John Nguyen <johnhvn94@gmail.com>
2019-03-22 19:03:35 -0700

    Update views.py

--- finalproject/finalapp/views.py
+++ finalproject/finalapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import TaskType, Task
+from .forms import TaskForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
