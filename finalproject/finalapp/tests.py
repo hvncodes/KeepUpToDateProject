4f6eb3465ad222ad1fa73aa7b8ae2971061c6dfd
John Nguyen <johnhvn94@gmail.com>
2019-03-22 19:40:28 -0700

    Update tests.py

--- finalproject/finalapp/tests.py
+++ finalproject/finalapp/tests.py
@@ -22,3 +22,16 @@ Class TestIndex(TestCase):
   def test_view_users_correct_template(self):
     response=self.client(reverse('index'))
     self.assertTemplateUsed(response, 'finalapp/index.html')
+
+    
+    
+class New_Task_Form_Test(TestCase):
+
+    # Valid Form Data
+    def test_TaskForm_is_valid(self):
+        form = TaskForm(data={'taskname': "Check up", 'tasktype': "Monthly", 'user': "John", 'evententrydate': "2019-03-22", 'eventurl': "google.com", 'eventdescription': "words go here"})
+        self.assertTrue(form.is_valid())
+    # Invalid Form Data
+    def test_UserForm_invalid(self):
+        form = TaskForm(data={'taskname': "Check up", 'tasktype': "Monthly", 'user': "John", 'evententrydate': "2018-03-22", 'eventurl': "google.com", 'eventdescription': "task description goes here"})
+        self.assertFalse(form.is_valid())
