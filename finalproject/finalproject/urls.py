6b5d9db264a2e427bff35802c2f9e8ace4c46713
John Nguyen <johnhvn94@gmail.com>
2019-03-22 17:51:35 -0700

    Update urls.py

--- finalproject/finalproject/urls.py
+++ finalproject/finalproject/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('finalapp/', include('finalapp.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
