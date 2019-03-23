dbb4acd5d8341317bacca8642fef0057e89815c3
John Nguyen <johnhvn94@gmail.com>
2019-03-22 21:32:38 -0700

    Update tests.py

--- finalproject/finalapp/tests.py
+++ finalproject/finalapp/tests.py
@@ -124,9 +124,9 @@ class New_Task_Form_Test(TestCase):
 
     # Valid Form Data
     def test_TaskForm_is_valid(self):
-        form = TaskForm(data={'taskname': "Check up", 'tasktype': "Monthly", 'user': "John", 'taskentrydate': "2019-03-22", 'taskurl': "google.com", 'eventdescription': "words go here"})
+        form = TaskForm(data={'taskname': "Check up", 'tasktype': "Monthly", 'user': "John", 'taskentrydate': "2019-03-22", 'taskurl': "google.com", 'taskdescription': "words go here"})
         self.assertTrue(form.is_valid())
     # Invalid Form Data
     def test_UserForm_invalid(self):
-        form = TaskForm(data={'taskname': "Check up", 'tasktype': "Monthly", 'user': "John", 'taskentrydate': "2018-03-22", 'taskurl': "google.com", 'eventdescription': "task description goes here"})
+        form = TaskForm(data={'taskname': "Check up", 'tasktype': "Monthly", 'user': "John", 'taskentrydate': "2018-03-22", 'taskurl': "google.com", 'taskdescription': "task description goes here"})
         self.assertFalse(form.is_valid())
